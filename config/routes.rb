f84fc921dd215e3fb6a0321912dd47edad4419a6
Maxi Amodei <maxi_amodei@hotmail.com>
2021-03-01 23:49:31 -0300

    Youtube API not authorized yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Authenticate to youtube chanel
+  get '/youtube_sessions', to: 'youtube_sessions#new'
+  get '/youtube_sessions/callback', to: 'youtube_sessions#callback'
 end
