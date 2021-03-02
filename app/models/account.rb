e65c82437aa409d3d0382eaec1615a7b06cd27f8
Ray De Leon <radeleon31@gmail.com>
2021-03-02 13:59:59 -0500

    view and controller for publishing

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,3 @@
 class Account < ApplicationRecord
   validates :token, :platform_name, :uid, presence: true
-
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,3 @@
 class Channel < ApplicationRecord
-    validates :name, presence: true
+  validates :name, presence: true
 end
