158a3849b0603f612c7659bab19662459c4459dc
Jesus Huiza <jesus_huiza@hotmail.com>
2021-03-04 18:55:53 -0400

    Monetizer 04-03 V3

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,15 +1,19 @@
 class ChannelsController < ApplicationController
     def show
-      
     end
 
     def last_day
-        
+        cd1-valor.innerText = "71%"
+        cd1-porc.innerText = "75%"
+
     end
+
     def last_week
     end
+
     def last_month
     end
+
     def last_year
     end
 end
