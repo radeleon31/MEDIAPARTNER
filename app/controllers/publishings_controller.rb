724224d90a6455bc325507d3352af72862570d03
Maxi Amodei <maxi_amodei@hotmail.com>
2021-03-04 16:48:52 -0300

    Building model to refresh YT Videos

--- app/controllers/publishings_controller.rb
+++ app/controllers/publishings_controller.rb
@@ -42,9 +42,45 @@ class PublishingsController < ApplicationController
   #     render :index
   #   end
   # end
+  def update_publishings
+    @data = FetchYoutubeVideos.call(current_user.youtube_sessions.last)
+    
+    {
+      "video_id" => { likes: 20, views: 40 },
+      "video_id_2" => { likes: 20, views: 40 }
+    }
+    @videos = FetchYoutubeVideos.call(current_user.youtube_sessions.last)
+      @videos.each do |video_hash|
+        publishing = Publishing.find_or_initialize_by(uid: video.id.video_id)
+        if publishing.id.nil? # Si es nil ===> tengo que crearlo
+          publishing.title = video_hash[:title]
+          publishing.description = video_hash[:description]
+          publishing.thumbnail = video_hash[:thumbnail]
+          publishing.channel_id = video_hash[:channel_id]
+          publishing.status = "Published from Youtube"
+          publishing.user = current_user
+          publishing.uid = video_hash[:id]
+        end
+        # Tanto SI existe como si NO existe, quiero actualizar los likes y viwes y guardarlo
+        publishing.likes = @data[publishing.uid][:likes]
+        publishing.views = @data[publishing.uid][:views]
+        publishing.save
+      end
+      #notice: ""
+      redirect_to overview_path
+  end
+
+  # {
+  #   title: video.snippet.title,
+  #   description: video.snippet.description,
+  #   thumbnail: video.snippet.thumbnails.default.url,
+  #   id: video.id.video_id,
+  #   channel_id: video.snippet.channel_id
+  # }
 
   def overview
     # Este Array es para no agotar quotas a la API
+    # @publishings = current_user.publishings
       @videos = [{
         title: "test",
         description: "test",
--- app/models/fetch_youtube_analytics.rb
+++ app/models/fetch_youtube_analytics.rb
@@ -11,33 +11,22 @@ class FetchYoutubeAnalytics
       "rt:activeUsers", # Metrics
       options: { authorization: @auth_client}
       )
+      analytics.query_report(
+        options: { authorization: @auth_client},
+        dimensions:"video",
+        end_date:"2021-03-03", # Test Only - Cambiar a fecha actual
+        ids:"channel==MINE",
+        max_results:10, # Test Only - Cambiar a todos. paginacion?
+        metrics:"views,likes",
+        sort:"-views",
+        start_date:"2019-01-01" # Test Only - Cambiar a variable elegida o predefinida restandole a fecha actual
+      )
     end
   end
-  # dimensions:"video",endDate:"2021-01-01",ids:"channel==MINE",maxResults:10,metrics:"views,likes",sort:"-views",startDate:"2010-01-01"
   # 1) require 'google/apis/youtube_v3'
   # 2) require 'google/apis/youtube_analytics_v2'
   # 3) @auth_client = Signet::OAuth2::Client.new(YoutubeSession.last.credentials)
   # 4) analytics = Google::Apis::YoutubeAnalyticsV2::YouTubeAnalyticsService.new
-  # 5) analytics.query_report(options: { authorization: @auth_client},dimensions:"video",end_date:"2021-03-03",ids:"channel==MINE",max_results:10,metrics:"views,likes",sort:"-views",start_date:"2020-01-01")
-  
-
-
-
-
-
-  # analytics.query_report(options: { authorization: @auth_client},
-  # ids: 'channel==MINE',
-  #metrics:'views',end_date:'2021-02-01',
-  # start_date:'2020-02-01')
-
-
-  # ERROR:
-  # Google::Apis::ClientError: insufficientPermissions: Request had insufficient authentication scopes.
-  # YA SE AMPLIARON LOS SCOPES Y ME PIDIERON LOS PERMISOS
-
-  #ERROR
-  # Google::Apis::ClientError: accessNotConfigured: Google Analytics API has not been used in project 957706471560 
-  # before or it is disabled. Enable it by visiting 
-  # https://console.developers.google.com/apis/api/analytics.googleapis.com/overview?project=957706471560 then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.
-  # Google::Apis::ClientError: insufficientPermissions: User does not have any Google Analytics account
+  # 5) analytics.query_report(options: { authorization: @auth_client},dimensions:"video",end_date:"2021-03-03",ids:"channel==MINE",max_results:10,metrics:"views,likes",sort:"-views",start_date:"2019-01-01")
+end
   
