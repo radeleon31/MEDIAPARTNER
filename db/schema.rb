d0e7a10067533c4113e34629d6899eaf73c74e08
Maxi Amodei <maxi_amodei@hotmail.com>
2021-03-05 15:13:26 -0300

    changes in db

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_03_05_014542) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_channels_on_user_id"
   end
 
   create_table "publishings", force: :cascade do |t|
@@ -63,6 +65,8 @@ ActiveRecord::Schema.define(version: 2021_03_05_014542) do
     t.integer "likes"
     t.integer "views"
     t.string "thumbnail"
+    t.bigint "channels_id"
+    t.index ["channels_id"], name: "index_publishings_on_channels_id"
     t.index ["user_id"], name: "index_publishings_on_user_id"
   end
 
@@ -91,6 +95,8 @@ ActiveRecord::Schema.define(version: 2021_03_05_014542) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "channels", "users"
+  add_foreign_key "publishings", "channels", column: "channels_id"
   add_foreign_key "publishings", "users"
   add_foreign_key "youtube_sessions", "users"
 end
