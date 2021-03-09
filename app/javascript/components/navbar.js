c243a17a0e73453592acb1b1cf054a913a70c028
Maxi Amodei <maxi_amodei@hotmail.com>
2021-03-09 00:57:00 -0300

    navbar-fixed

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,9 +1,11 @@
 const toggleNavbar = () => {
   const navToggle = document.querySelector(".nav-toggle");
-  const links = document.querySelector(".links");
+  if (navToggle) {
+    const links = document.querySelector(".links");
 
-  navToggle.addEventListener("click", function () {
-    links.classList.toggle("show-links");
-  });
+    navToggle.addEventListener("click", function () {
+      links.classList.toggle("show-links");
+    });
+  }
 }
 export { toggleNavbar };
