12c8b9dadcf29c0201f847b6cb8ec28362dc69d4
Ray De Leon <radeleon31@gmail.com>
2021-03-10 08:09:09 -0500

    updated account and post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("channels")
 require("chartkick")
 require("chart.js")
 //= require chartkick
+//= require bootstrap/bootstrap-tooltip
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
